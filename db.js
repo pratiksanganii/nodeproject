3f9a6e49c8515cad6120bd7e403f392b478a1cb7
Pratik <pratikpsangani03@gmail.com>
2023-02-05 12:35:02 +0530

    connection string options removed

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const mongodb = require('mongodb')
 
-mongodb.MongoClient.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err,client){
+mongodb.MongoClient.connect(process.env.CONNECTIONSTRING, function(err,client){
     module.exports=client
     const app = require('./app')
     app.listen(process.env.PORT)
